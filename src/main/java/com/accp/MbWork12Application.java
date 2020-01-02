95cf18aee8882c8227a26677a8c87ae326845f8e
Zo <2622534795@qq.com>
2020-01-02 10:04:48 +0800

    第一次提交代码库

---
+++ src/main/java/com/accp/MbWork12Application.java
@@ -0,0 +1,13 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbWork12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbWork12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/accp/MbWork12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbWork12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
